120282ece597d4be89a7e76f211267940bcdb092
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-05-01 00:13:16 -0400

    Added design controller and views

---
+++ app/controllers/designs_controller.rb
@@ -0,0 +1,58 @@
+class DesignsController < ApplicationController
+  before_action :set_design, only: [:show, :edit, :update, :destroy]
+
+  # GET /designs
+  def index
+    @designs = Design.all
+  end
+
+  # GET /designs/1
+  def show
+  end
+
+  # GET /designs/new
+  def new
+    @design = Design.new
+  end
+
+  # GET /designs/1/edit
+  def edit
+  end
+
+  # POST /designs
+  def create
+    @design = Design.new(design_params)
+
+    if @design.save
+      redirect_to @design, notice: 'Design was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  # PATCH/PUT /designs/1
+  def update
+    if @design.update(design_params)
+      redirect_to @design, notice: 'Design was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  # DELETE /designs/1
+  def destroy
+    @design.destroy
+    redirect_to designs_url, notice: 'Design was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_design
+      @design = Design.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def design_params
+      params[:design]
+    end
+end
