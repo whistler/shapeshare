acab403492de663023710246d2640a42d45ae539
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-04-30 23:15:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThreeDPrinting::Application.config.secret_key_base = '6d35ceba80a718e79bd2168366cbd068fe9ca7ddef5fafadf0fbec6a3778a5f9752384c91afd544aca7f97dae8d9ef67f33004a1328a9f9127dfeb36004e008b'
